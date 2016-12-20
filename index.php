ae3c25e73860dbe651c028a8fab9f518be93a69e
Miranda <mlakis@u.rochester.edu>
2016-12-20 14:59:22 -0500

    finished content for index
    other small changes besides that

--- index.php
+++ index.php
@@ -10,12 +10,96 @@
 			<?php include "inc/nav.inc"; ?>
 
 			<!-- Z-Pattern Content -->
+			<section id="z">
+				<h1>Overview of Accomplishments</h1> .
 
+				<div class="content">
+					<table>
+						<tr>
+							<td>
+								<a href="http://www.uofrathletics.com/roster.aspx?rp_id=3983"><img src="images/lakis.jpg" alt="Miranda Lakis"/></a>
+							</td>
+							<td>
+								<h2>Miranda Lakis</h2>
+								<p>
+									Miranda is a current sophomore at the University of Rochester. She is a defenseman starter for the Varisty Field Hockey Team, helping the squad reach the NCAA tournament two years in a row. Link to her roster page can be found <a href="http://www.uofrathletics.com/roster.aspx?rp_id=3983">here</a>.
+								</p>
+							</td>
+						</tr>
 
+						<tr>
+							<td>
+								<img src="images/library.jpg" alt="Rush Rhees"/>
+							</td>
+							<td>
+								<h2>Computer Science Undergrad</h2>
+								<p>
+									She is also a computer science undergrad student. Last spring, she made the Dean's List, and this fall she was named to the <a href="http://www.uofrathletics.com/news/2016/11/3/30-named-to-liberty-league-all-academic-team.aspx">Liberty League All-Academic Team</a> along with eleven other teammates.
+								</p>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<a href="http://media.democratandchronicle.com/agr/main/article/59"><img src="images/rochester.jpg" alt="AGR"/></a>
+							</td>
+							<td>
+								<h2>All-Greater Rochester Player of the Year</h2>
+								<p>
+									After finishing her senior season in high school, Miranda was received the All-Greater Rochester Player of the Year award in 2014 due to her successful high school career. As captain, she helped her team win two sectional titles for the first time in 14 years while also making the state tournament both her junior and senior year. Link to the full article can be found <a href="http://media.democratandchronicle.com/agr/main/article/59">here</a>.
+								</p>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</section>
 
 			<!-- F-Pattern Content -->
+			<section id="f">
+				<h1>About Me</h1>
+
+				<div class="content">
+					<table>
+						<tr>
+							<td>
+								<img src="images/city.jpg" alt="City of Rochester"/>
+							</td>
+							<td>
+								<h2>Rochester Native</h2>
+								<p>
+									Miranda Lakis was born in Rochester, NY. She has lived here to her whole life.
+								</p>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<img src="images/highschool.jpg" alt="Brighton High School"/>
+							</td>
+							<td>
+								<h2>Brighton High School Graduate</h2>
+								<p>
+									Miranda graduated from Brighton High School in 2015 with honors. She is continuing her education at the University of Rochester.
+								</p>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<img src="images/futures.jpg" alt="USA Futures Logo"/>
+							</td>
+							<td>
+								<h2>Work Experience</h2>
+								<p>
+									Throughout high school, Miranda coached several youth field hockey clinics (typically around ages 8-12). Once in college, Miranda began coaching in the USA Futures program that is aimed for kids between the ages of 13 and 18. In addition to her coaching, Miranda has also worked in an afterschool preschool/kindergarten program for Seton Catholic School.
+								</p>
+							</td>	
+						</tr>
 
 
+					</table>
+				</div>
+			</section>
 
 
 		</div>
